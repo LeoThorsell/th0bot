1e64b53b38c21907f9e1190241f1600ca9081664
Emil Österlund <emil@larsemil.se>
2016-04-06 12:23:29 +0200

    Fixed a small bug in the eastereggs

--- modules/pull.js
+++ modules/pull.js
@@ -11,13 +11,15 @@ mm.add({
             message = message.trim();
             if(message.startsWith('!pull pork'))
                 me.irc.say(to, "I pulled that pork and served it to a dork!");
+            return;
             if(message.startsWith('!pull finger')){
                 me.irc.say(to, "You sure you want to do that?");
                 me.irc.say(to, "                 prrrpt.");
+                return;
             }
             if(message.startsWith('!pull'))
                 me.irc.say(to, me.irc.opt.nick + ' tar gärna imot pull requests. Titta här: https://github.com/LeoThorsell/th0bot/');
-            
+            return
         });
     }
 });

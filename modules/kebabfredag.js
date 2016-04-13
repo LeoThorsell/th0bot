519b655d2f5590e3434d9df5d10932f71354dcab
Jon Börjesson <jon@spuun.se>
2016-04-13 13:01:16 +0200

    Fixed identation

--- modules/kebabfredag.js
+++ modules/kebabfredag.js
@@ -1,12 +1,11 @@
 require('../leoLibrary.js');
 var mm =  require('../moduleManager.js');
-var Curl = require('node-libcurl').Curl
+var Curl = require('node-libcurl').Curl;
 var triggerExpr = /^!kebab(fredag)?/;
 
 mm.add({
 	name: 'kebabfredag',
-	init: function(){
-		// LeoTho => #leotho: hej
+	init: function() {
 		var me = this;
 		this.on('message', function (from, to, message) {
 			if(from == me.irc.opt.nick)
@@ -16,19 +15,18 @@ mm.add({
 				return;
 			var curl = new Curl();
 			url = "http://ere.kebabfredag.nu/api/ere";
-			curl.setOpt( Curl.option.FOLLOWLOCATION, true);
-			curl.setOpt( Curl.option.HTTPHEADER, ['Accept: application/json']);
+			curl.setOpt(Curl.option.FOLLOWLOCATION, true);
+			curl.setOpt(Curl.option.HTTPHEADER, ['Accept: application/json']);
 			curl.setOpt('URL', url);
-			curl.on( 'end', function( statusCode, body, headers ) {
+			curl.on( 'end', function(statusCode, body, headers) {
 				var data = JSON.parse(body);
 				if (data && data.hasOwnProperty('answer')) {
 					var response = data.answer ? 'Jepp, det är kebabfredag.' : 'Nepp, inte kebabfredag idag :(';
-							me.irc.say(to, from + ': ' + response);
-							}
-							});
-					curl.on('error', function(){curl.close();});
-					curl.perform();
-
-					});
+					me.irc.say(to, from + ': ' + response);
+				}
+			});
+			curl.on('error', function(){curl.close();});
+			curl.perform();
+		});
 	}
 });

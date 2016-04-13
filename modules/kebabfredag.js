5ecbcba7454ff00c1a17fd4e10dab54587aa31e9
Jon Börjesson <jon@spuun.se>
2016-04-13 12:58:09 +0200

    Updated readme Added !kebabfredag module

---
+++ modules/kebabfredag.js
@@ -0,0 +1,34 @@
+require('../leoLibrary.js');
+var mm =  require('../moduleManager.js');
+var Curl = require('node-libcurl').Curl
+var triggerExpr = /^!kebab(fredag)?/;
+
+mm.add({
+	name: 'kebabfredag',
+	init: function(){
+		// LeoTho => #leotho: hej
+		var me = this;
+		this.on('message', function (from, to, message) {
+			if(from == me.irc.opt.nick)
+				return;
+			message = message.trim();
+			if(!triggerExpr.test(message))
+				return;
+			var curl = new Curl();
+			url = "http://ere.kebabfredag.nu/api/ere";
+			curl.setOpt( Curl.option.FOLLOWLOCATION, true);
+			curl.setOpt( Curl.option.HTTPHEADER, ['Accept: application/json']);
+			curl.setOpt('URL', url);
+			curl.on( 'end', function( statusCode, body, headers ) {
+				var data = JSON.parse(body);
+				if (data && data.hasOwnProperty('answer')) {
+					var response = data.answer ? 'Jepp, det är kebabfredag.' : 'Nepp, inte kebabfredag idag :(';
+							me.irc.say(to, from + ': ' + response);
+							}
+							});
+					curl.on('error', function(){curl.close();});
+					curl.perform();
+
+					});
+	}
+});

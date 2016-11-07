4a7cef0687635358462b540d09bf7bc497786c1a
Jon <jon@spuun.se>
2016-11-07 13:31:13 +0100

    Updated haddock to new irc lib

--- modules/haddock.js
+++ modules/haddock.js
@@ -5,33 +5,33 @@ var HtmlEntities = require('html-entities').AllHtmlEntities;
 
 mm.add({
 	name: 'haddock',
-	init: function(){
+	init: function(irc){
 		var htmlDecoder = new HtmlEntities();
-		var me = this;
-		this.on('message', function (from, to, message) {
-                    if(me.irc == null )
-                        return;
-                    if(from == me.irc.opt.nick)
-                        return;
-                    message = message.trim();
-                    if(message.startsWith('!haddock')){
-                        var vendor = '';
-                        var curl = new Curl();
-                        curl.setOpt('FOLLOWLOCATION', true );
-                        curl.setOpt('URL', "https://haddock.updog.se/");
-                        curl.on( 'end', function( statusCode, body, headers ) {
-                            console.log(body);
-                            var haddockMatch = /<h1 class="title">([\s\S]+?)<\/h1>/gi.exec(body);
-                            if(haddockMatch == null){
-                                return
-                            }
-                            var haddock = htmlDecoder.decode(haddockMatch[1]);
-                            me.irc.say(to, haddock);
+		irc.on('chancmd:haddock', function (from, channel, message, target) {
+			var uinfo = irc.tools.parseUserinfo(from);
+			var say = (msg) => {
+				if (uinfo.nick == target) {
+					irc.send(`PRIVMSG ${channel} :${msg}`);
+				else
+					irc.send(`PRIVMSG ${channel} :${target}: ${msg}`);
+				}
+			};
+			if(uinfo.nick == irc.connection.nick)
+				return;
+			var curl = new Curl();
+			curl.setOpt('FOLLOWLOCATION', true );
+			curl.setOpt('URL', "https://haddock.updog.se/");
+			curl.on( 'end', function( statusCode, body, headers ) {
+				var haddockMatch = /<h1 class="title">([\s\S]+?)<\/h1>/gi.exec(body);
+				if(haddockMatch == null){
+					return
+				}
+				var haddock = htmlDecoder.decode(haddockMatch[1]);
+				say(haddock);
 
-                        });
-                        curl.on('error', function(){curl.close();});
-                        curl.perform();
-                    }
+			});
+			curl.on('error', function(){curl.close();});
+			curl.perform();
 		});
 	}
 });

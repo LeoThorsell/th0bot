6bd7f49b498ef925f1480040706451bc6ba5c825
Jon <jon@spuun.se>
2016-11-07 12:24:49 +0100

    Change irc lib

--- bot.js
+++ bot.js
@@ -1,9 +1,29 @@
-var irc = require('irc');
+var irc = require('jonirc');
 var mm = require('./moduleManager.js');
 
-var client = new irc.Client('irc.freenode.net', 'Th0bot', {
-	channels: ['#leotho', '#himat', '#daladevelop'],
+var connectionAttempt = 0;
+var channels = ['#callide'];
+
+
+var client = irc('irc.freenode.net', 'Th0botta', {
 	userName: 'Leos',
-    	realName: 'Owned by LeoTho'
+  realName: 'Owned by LeoTho'
+});
+client.connect();
+
+client.on('disconnected', () => {
+	setTimeout(client.connect, (Math.min(connectionAttempt++,6))*10*1000);  
+});
+client.on('connected', () => {
+	connectionAttempt = 0;
+	channels.forEach((channel) => {
+		client.send(`JOIN ${channel}`);
+	});
 });
 mm.init(client);
+
+
+// backward compatibility stuff
+client.say = function(target, msg) {
+	client.send(`PRIVMSG ${target} :${msg}`);
+}
--- moduleManager.js
+++ moduleManager.js
@@ -1,8 +1,8 @@
 var domain = require('domain');
 if (typeof String.prototype.endsWith !== 'function') {
-    String.prototype.endsWith = function(suffix) {
-        return this.indexOf(suffix, this.length - suffix.length) !== -1;
-    };
+	String.prototype.endsWith = function(suffix) {
+		return this.indexOf(suffix, this.length - suffix.length) !== -1;
+	};
 }
 
 var moduleManager = {
@@ -13,12 +13,6 @@ var moduleManager = {
 		if(!module.name)
 			return;
 		module.moduleManager = this;
-		module.on = function(eventName, cb){
-			if(eventName == 'pm')
-				module.pmCallback = cb;
-			else if(eventName == 'message')
-				module.messageCallback = cb;
-		};
 		this.modules[module.name] = module;
 	},
 	init: function(irc, path){
@@ -30,11 +24,11 @@ var moduleManager = {
 				return;
 			console.log(path + file);
 			var d = domain.create();
-			var name = file.substring(3);
+			var name = file;
 			d.on('error', function(err){
 				console.log(file + ' ' + err);
 				if (err.stack)
-				console.log(err.stack);
+					console.log(err.stack);
 				var module = mm.modules[name];
 				if(module == null)
 					return;
@@ -42,7 +36,13 @@ var moduleManager = {
 				console.log(name + ' removed from manager');
 			});
 			d.run(function(){
-				require(path + file);
+				try {
+					require(path + file);
+				} catch (e) {
+					console.error(`Error loading module ${name}: ${e}`);
+					if (e.stack)
+						console.error(e.stack);
+				}
 			});
 		});
 		for(var name in this.modules){
@@ -51,86 +51,58 @@ var moduleManager = {
 			console.log('Loading module ' + name );
 			if(typeof(module.init) == 'function'){
 				console.log('Calling init()');
-				module.init();	
-			}
-		}
-		var me = this;
-		irc.addListener('pm', function(from, msg){
-			me.privateMessage(from, msg);
-		});	
-		irc.addListener('message', function(from, to, msg){
-			me.message(from, to, msg);
-		});
-	},
-	privateMessage: function(from, msg){
-		for(var name in this.modules){
-			var module = this.modules[name];
-			if( typeof(module.pmCallback) != 'function')
-				continue;
-			try
-			{
-				module.pmCallback(from, msg);
-			}catch(e){
-				console.log( 'Module ' + name + ' on pm: ' + e);
-			}
-		}	
-	},
-	message: function(from, to, message){
-		for(var name in this.modules){
-			var module = this.modules[name];
-			if(typeof(module.messageCallback)!='function')
-				continue;
-			try
-			{
-				module.messageCallback(from, to, message);
-			}catch(e){
-				console.log('Module ' + name + ' on message: ' + e);
+				try {
+					module.init(irc);	
+				} catch (e) {
+					console.error(`Error initializing module ${name}: ${e}`);
+					if (e.stack)
+						console.error(e.stack);
+				}
 			}
 		}
 	}
 };
 module.exports =  moduleManager;
-moduleManager.add({
-	name: 'ModuleManager',
-	init: function(){
-		var me = this;
-		this.on('pm', function(from, msg){
-			if(from != 'LeoTho')
-				return;
-			if(msg[0]!= '!')
-				return;
-			console.log('mm on pm');
-			var splitted = msg.split(' ');
-			var action = splitted[0];
-			action = action.substring(1);
-			var argument = splitted.splice(1, Number.MAX_VALUE).join(' ');
-			me.performAction(action, argument);
-		});	
-	},
-	performAction: function(action, argument){
-		console.log('performAction');
-		if(action == "reload"){
-			for( var name in this.moduleManager.modules){
-				if( name != argument)
-					continue;
-				delete this.moduleManager.modules[name];
-				console.log('delete done');
-			}
-			var name = argument;
-			delete require.cache[require.resolve('./modules/' + name + '.js')];
-			try{
-				console.log('./modules/' + name + '.js');
-				require('./modules/' + name + '.js');
-			}
-			catch(e){
-				console.log(e);
-			}
-			var module = this.moduleManager.modules[name];
-			if(module == null)
-				return;
-			module.irc = this.irc;
-			module.init();
-			console.log('init done');
-		}
-	}
-});
+/*moduleManager.add({
+name: 'ModuleManager',
+init: function(){
+this.irc.on('botmsg', function(from, msg){
+if(this.irc.tools.parseUserinfo(from).nick != 'LeoTho')
+return;
+if(msg[0]!= '!')
+return;
+console.log('mm on pm');
+var splitted = msg.split(' ');
+var action = splitted[0];
+action = action.substring(1);
+var argument = splitted.splice(1, Number.MAX_VALUE).join(' ');
+me.performAction(action, argument);
+});	
+},
+performAction: function(action, argument){
+console.log('performAction');
+if(action == "reload"){
+for( var name in this.moduleManager.modules){
+if( name != argument)
+continue;
+delete this.moduleManager.modules[name];
+console.log('delete done');
+}
+var name = argument;
+delete require.cache[require.resolve('./modules/' + name + '.js')];
+try{
+console.log('./modules/' + name + '.js');
+require('./modules/' + name + '.js');
+}
+catch(e){
+console.log(e);
+}
+var module = this.moduleManager.modules[name];
+if(module == null)
+return;
+module.irc = this.irc;
+module.init();
+console.log('init done');
+}
+}
+});*/

1690d55600fd2105576d5a43a26f7b9bf6dc4dc8
Jon Börjesson <jon@spuun.se>
2018-09-07 10:48:40 +0200

    Possibility to forward !kebab and !kebabfredag response

--- modules/kebabfredag.js
+++ modules/kebabfredag.js
@@ -1,36 +1,50 @@
 require('../leoLibrary.js');
 var http = require('../http.js');
 var mm =  require('../moduleManager.js');
-var commandTriggerExpr = /^!kebab(fredag)?/;
 var questionTriggerExpr = /^[äe]re?\s+(det?\s+)?kebabfredag\?[?!\s]*$/i;
 var kebabApiUrl = "http://ere.kebabfredag.nu/api/";
 
 mm.add({
-	name: 'kebabfredag',
-	init: function(irc) {
-		irc.on('chanmsg', (from, channel, message) => {
-			var uinfo = irc.tools.parseUserinfo(from);
-			if(uinfo.nick == irc.connection.nick)
-				return;
-			message = message.trim();
-			if(!commandTriggerExpr.test(message) && !questionTriggerExpr.test(message))
-				return;
-			var url = kebabApiUrl + "ere";
-			http
-				.get(url, {headers:{'Accept':'application/json'}})
-				.then((res) => {
-					var data;
-					try {
-						data = JSON.parse(res.body);
-					} catch (e) {
-						console.log('Lol ere.kebabfredag.nu\'s api är trasigt!', e);
-						return;
-					}
-					if (data && data.hasOwnProperty('answer')) {
-						var response = data.answer ? 'Jepp, det är kebabfredag.' : 'Nepp, inte kebabfredag idag :(';
-						irc.send(`PRIVMSG ${channel} :${uinfo.nick}: ${response}`);
-					}
-				});
-		});
-	}
+  name: 'kebabfredag',
+  init: function(irc) {
+
+    function kebabQuery(cb) {
+      var url = kebabApiUrl + "ere";
+      http
+        .get(url, {headers:{'Accept':'application/json'}})
+        .then((res) => {
+          var data;
+          try {
+            data = JSON.parse(res.body);
+          } catch (e) {
+            console.log('Lol ere.kebabfredag.nu\'s api är trasigt!', e);
+            return;
+          }
+          if (data && data.hasOwnProperty('answer')) {
+            cb(data.answer ? 'Jepp, det är kebabfredag.' : 'Nepp, inte kebabfredag idag :(');
+          } else {
+            cb();
+          }
+        });
+    }
+    function onCmd(from, channel, message, target, respond) {
+      kebabQuery(res => {
+        res && respond(res);
+      });
+    }
+
+    irc.on('chancmd:kebab', onCmd);
+    irc.on('chancmd:kebabfredag', onCmd);
+    irc.on('chanmsg', (from, channel, message, target, respond) => {
+      var uinfo = irc.tools.parseUserinfo(from);
+      if(uinfo.nick == irc.connection.nick)
+        return;
+      message = message.trim();
+      if(!questionTriggerExpr.test(message))
+        return;
+      kebabQuery(res => {
+        irc.send(`PRIVMSG ${channel} :${uinfo.nick}: ${res}`);
+      })
+    });
+  }
 });
